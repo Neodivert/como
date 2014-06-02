39c10c77bd0c376609eb03c3c92295d8d893a72a
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2014-06-02 17:44:59 +0100

    - Modified license advice so now the project is under the GPL v3 license, without the "any later version" clause.

--- src/common/commands/drawable_commands/light_creation_command.cpp
+++ src/common/commands/drawable_commands/light_creation_command.cpp
@@ -4,9 +4,8 @@
  * This file is part of COMO.
  *
  * COMO is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
  *
  * COMO is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
