4eb3f8f2594097ba9197efffb11508d9375941f0
moisesjbc <moisesjbc@gmail.com>
2014-10-15 11:43:52 +0100

    - Added a TODO.

--- src/client/models/3d/textures/texture.cpp
+++ src/client/models/3d/textures/texture.cpp
@@ -46,6 +46,7 @@ Texture::Texture( const ResourceID& id, const std::string& name, const TextureIn
     glActiveTexture( GL_TEXTURE0 );
     glBindTexture( GL_TEXTURE_2D, oglName_ );
 
+    // TODO: Change to GL_LINEAR?
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
