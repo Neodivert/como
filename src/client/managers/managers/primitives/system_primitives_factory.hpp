a4f755da6b2f449cff09dfe049441b51151cbf92
moisesjbc <moisesjbc@gmail.com>
2014-11-17 13:00:03 +0000

    Now default spheres are created with radius = 0.5f.

--- src/client/managers/managers/primitives/system_primitives_factory.hpp
+++ src/client/managers/managers/primitives/system_primitives_factory.hpp
@@ -55,7 +55,7 @@ class SystemPrimitivesFactory : public ServerWriter, public Lockable
         ResourceID createCube( float width = 1.0f, float height = 1.0f, float depth = 1.0f );
         ResourceID createCone( float height = 1.0f, float radius = 0.5f, std::uint16_t nBaseVertices = 32 );
         ResourceID createCylinder( float height = 1.0f, float radius = 0.5f, std::uint16_t nRadialVertices = 32 );
-        ResourceID createSphere( float radius = 1.0f, std::uint16_t nDivisions = 32 );
+        ResourceID createSphere( float radius = 0.5f, std::uint16_t nDivisions = 32 );
 
 
         /***
