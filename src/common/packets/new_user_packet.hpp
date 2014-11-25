b22e7eb1752ddcfa4eb01e592cbe6d264200d940
moisesjbc <moisesjbc@gmail.com>
2014-11-25 17:33:32 +0000

    Removed doxygen comments from class NewUserPacket.

--- src/common/packets/new_user_packet.hpp
+++ src/common/packets/new_user_packet.hpp
@@ -33,72 +33,43 @@ namespace como {
 class NewUserPacket : public Packet
 {  
     public:
-        /*! Name of the user trying to connect to server */
+        // Name of the user trying to connect to server
         PackableString name_;
 
     public:
         /***
          * 1. Construction
          ***/
-
-        /*! \brief Default constructor. */
         NewUserPacket();
-
-        /*! \brief Constructs a NewUserPacket with the given user name. */
         NewUserPacket( const char* name );
-
-        /*! \brief Copy constructor. */
         NewUserPacket( const NewUserPacket& b );
-
-        /*! \brief Move constructor. */
         NewUserPacket( NewUserPacket&& ) = delete;
-
-        /*! \brief See Packet::clone const. */
-        virtual Packet* clone() const ; // TODO: Memory leack?
+        virtual Packet* clone() const;
 
 
         /***
          * 2. Destruction
          ***/
-
-        /*! \brief Destructor. */
         virtual ~NewUserPacket() = default;
 
 
         /***
          * 3. Getters
          ***/
-
-        // TODO: Move to another "section" (no "Getters")?
-        /*! \brief See Packet::expectedType const */
         virtual bool expectedType() const;
-
-        /*!
-         * \brief Returns the user name field inside this packet.
-         * \return a std::string with the user name written in this packet.
-         */
         const char* getName() const ;
 
 
         /***
          * 4. Setters
          ***/
-
-        /*!
-         * \brief Sets the user name field of this packet.
-         * \param name new user name.
-         */
         void setName( const char* name );
 
 
         /***
          * 5. Operators
          ***/
-
-        /*! \brief Copy assignment operator */
         NewUserPacket& operator = ( const NewUserPacket& b );
-
-        /*! \brief Move assignment operator */
         NewUserPacket& operator = ( NewUserPacket&& ) = delete;
 };
 
